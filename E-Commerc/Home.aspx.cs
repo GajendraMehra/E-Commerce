cbcbbefeb093aa4061f8884029324fb84253a828
DESKTOP-KKDNG7M\Gajendra <Gajendramehra14rgnv@gmail.com>
2017-07-28 13:10:49 +0530

    admin panel complited

--- E-Commerc/Home.aspx.cs
+++ E-Commerc/Home.aspx.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
 
 public partial class Home : System.Web.UI.Page
 {
+
+    SqlCommand cmd;
+    SqlDataAdapter sd;
+    DataTable dt;
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
 
